665af7a5c87065318af447cde55d3c0b3d5cf6a9
maciej.fuhrmann <maciej.fuhrmann@microsoft.wsei.edu.pl>
2022-04-09 13:01:24 +0200

    Add project files.

---
+++ fractions/Fraction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractions
+{
+    class Fraction : IComparable<Fraction>
+    {
+        public readonly int nominator;
+        public readonly int denominator;
+
+        public Fraction(int nominator, int denominator)
+        {
+            if(denominator == 0)
+            {
+                throw new Exception("Denominator cannot be 0");
+            }
+            this.nominator = nominator;
+            this.denominator = denominator;
+        }
+
+        public Fraction Add(Fraction secondFraction)
+        {
+            return new Fraction(nominator * secondFraction.denominator + secondFraction.nominator * denominator, denominator * secondFraction.denominator);
+        }
+
+        public int CompareTo(Fraction fraction)
+        {
+            int result = nominator * fraction.denominator - fraction.nominator * denominator;
+
+            return (result > 0)? 1 : 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{nominator}/{denominator}";
+        }
+
+        public static Fraction operator +(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.nominator * fr2.denominator + fr2.nominator * fr1.denominator, fr1.denominator * fr2.denominator);
+        }
+
+        public static Fraction operator -(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.nominator * fr2.denominator - fr2.nominator * fr1.denominator, fr1.denominator * fr2.denominator);
+        }
+
+        public static Fraction operator *(Fraction fr1, Fraction fr2)
+        {
+            return new Fraction(fr1.nominator * fr2.nominator, fr1.denominator * fr2.denominator);
+        }
+
+        public static Fraction operator /(Fraction fr1, Fraction fr2)
+        {
+            if (fr2.nominator == 0)
+            {
+                throw new ArgumentException("Substraction is not possible");
+            }
+            return new Fraction(fr1.nominator * fr2.denominator, fr1.denominator * fr2.nominator);
+        }
+    }
+}
---
+++ fractions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fractions
+{
+    class Compare : IComparer<Fraction>
+    {
+        int IComparer<Fraction>.Compare(Fraction x, Fraction y)
+        {
+            int result = x.nominator * y.denominator - y.nominator * x.denominator;
+
+            return (result > 0) ? 1 : 0;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr1 = new Fraction(1, 3);
+            Fraction fr2 = new Fraction(1, 5);
+
+            Console.WriteLine(fr1.Add(fr2));
+            Console.WriteLine(fr1 * fr2);
+            Console.WriteLine(fr1 / fr2);
+
+            List<Fraction> list = new List<Fraction>();
+            list.Add(fr1);
+            list.Add(fr2);
+            list.Add(new Fraction(4, 5));
+            foreach (var item in list)
+            {
+            Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+            list.Sort(new Compare());
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
